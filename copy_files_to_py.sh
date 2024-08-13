1ad8773b036930690184837f205b142d539674f6
Nico Päller <n.p.98@hotmail.de>
2024-08-13 20:39:29 +0200

    add sending schematics to pi

--- copy_files_to_py.sh
+++ copy_files_to_py.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x #echo on
 
-rsync -rav -e ssh . admin@192.168.86.28:/home/admin/dev/chip_counter/ --include="*.py" --include='*.ttf' --include='*.svg' --include='*.png' --include='*.toml' --include='*.qss' --include='*.sh' --exclude='__pycache__/*' --exclude='.git/*' --exclude='.idea/*' --exclude='.ruff_cache/*' --exclude='*.s*'
+rsync -rav -e ssh . admin@192.168.86.28:/home/admin/dev/chip_counter/ --include="*.py" --include='*.ttf' --include='*.svg' --include='*.png' --include='*.toml' --include='*.qss' --include='*.sh' --exclude='__pycache__/*' --exclude='.git/*' --exclude='.idea/*' --exclude='.ruff_cache/*'
